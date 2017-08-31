dc679c4e54aa44e1478b7c37c530cd44d81b6be4
kqq411691175 <411691175@qq.com>
2017-08-31 11:32:01 +0800

    项目上传4.1
    2017年8月31日11:32:00

--- 360度旋转看图/js/index.js
+++ 360度旋转看图/js/index.js
@@ -9,14 +9,17 @@ Around.prototype.render = function(){
 }
 //根据鼠标偏移量修改背景图片
 Around.prototype.move = function(){
-	$('.container').on('mousemove',function(e){
-		for(var i=1;i<=18;i++){
-			if(e.offsetX>i*10 && e.offsetX<(i+1)*10){
-				$('.container').css('background-image','url(images/image1_'+i+'.jpg)');
+	$('.container').on('click',function(e){
+		$(this).on('mousemove',function(e){
+			for(var i=1;i<=18;i++){
+				if(e.offsetX>i*10 && e.offsetX<(i+1)*10){
+					$('.container').css('background-image','url(images/image1_'+i+'.jpg)');
+				}
+				if(e.offsetX>(i+18)*10 && e.offsetX<(i+19)*10){
+					$('.container').css('background-image','url(images/image1_'+i+'.jpg)');
+				}
 			}
-			if(e.offsetX>(i+18)*10 && e.offsetX<(i+19)*10){
-				$('.container').css('background-image','url(images/image1_'+i+'.jpg)');
-			}
-		}
+		})
+		
 	})
 }
