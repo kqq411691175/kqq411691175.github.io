d6de08c1c64022f0a78292cc5124505872bcc3ae
kqq411691175 <411691175@qq.com>
2017-08-31 11:56:00 +0800

    项目上传4.2
    2017年8月31日11:55:58

--- 360度旋转看图/js/index.js
+++ 360度旋转看图/js/index.js
@@ -9,8 +9,7 @@ Around.prototype.render = function(){
 }
 //根据鼠标偏移量修改背景图片
 Around.prototype.move = function(){
-	$('.container').on('click',function(e){
-		$(this).on('mousemove',function(e){
+		$('.container').on('click',function(e){
 			for(var i=1;i<=18;i++){
 				if(e.offsetX>i*10 && e.offsetX<(i+1)*10){
 					$('.container').css('background-image','url(images/image1_'+i+'.jpg)');
@@ -20,6 +19,4 @@ Around.prototype.move = function(){
 				}
 			}
 		})
-		
-	})
 }
